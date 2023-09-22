206ccfd5e2027a15eee9ab1c9bbebcba8ffd1b03
rortegaez <raulor83@gmail.com>
2023-09-22 11:55:00 +0200

    firts clase

--- Clase01/10.free-port.js
+++ Clase01/10.free-port.js
@@ -1,17 +1,21 @@
+/* net es como http pero más liviado
+
+*/
+
 const net = require('node:net')
 
 function findAvailablePort (desiredPort) {
   return new Promise((resolve, reject) => {
-    const server = net.createServer()
+    const server = net.createServer()// estamos creando el servidor
 
-    server.listen(desiredPort, () => {
+    server.listen(desiredPort, () => { // donde queremos que escuche al servidor
       const { port } = server.address()
-      server.close(() => {
-        resolve(port)
+      server.close(() => { // cerramos el servidor
+        resolve(port)// devolvemos el port
       })
     })
 
-    server.on('error', (err) => {
+    server.on('error', (err) => { // si no se puede abrir el port deseado, nos da esto codigo de error
       if (err.code === 'EADDRINUSE') {
         findAvailablePort(0).then(port => resolve(port))
       } else {
@@ -20,5 +24,9 @@ function findAvailablePort (desiredPort) {
     })
   })
 }
+
+/*
+Si el puerto es el deseado se abre, si el puerto no es el deseado, se ejecuta el server.on('error'), si el error es que el puerto esta ocupado, entonces asignamos el puerto 0 y esto nos dará el primer puerto libre y ser resolvera. Si el error fuera eso, nos devuelve el error
+*/
 
 module.exports = { findAvailablePort }
--- Clase01/9.http.js
+++ Clase01/9.http.js
@@ -1,15 +1,26 @@
+/* Un servidor puede hacer dos cosas, recibir una petición y lanzar una respuesta, esto es lo que hacemos aquí:
+requiremos http, que es el protocolo para servidores
+const server = ... levantamos el servidor donde lo indicamos req y res, recibir petición y lanzar respuesta
+server.listen que es para escuchar donde esta ejecutado el servidor */
+
 const http = require('node:http')
 const { findAvailablePort } = require('./10.free-port')
 
 const desiredPort = process.env.PORT ?? 3000
+/* process.env.PORT es una variable de entorno, entonces cuando queremos ejecutar el archivo devemos poner:
+PORT=1234 node http.js
+en este caso ejecuta intentando con el puerto que le hemos enviado y si no le hemos enviado lo intenta con el 3000
+*/
 
 const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hola Mundo')
 })
 
-findAvailablePort(desiredPort).then(port => {
+findAvailablePort(desiredPort).then((port) => {
   server.listen(port, () => {
     console.log(`server listening on port http://localhost:${port}`)
   })
 })
+
+// si ponemos como puerto el 0, nos va a asignar el primer puerto vacio que encuentre. y donde ahora tenemos ${port}, podemos poner ${server.address().port} y nos diría el puerto que se esta utilizando.
